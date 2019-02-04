77c10b25fe1c843776a907e400b3dfef1c7d8668
Aries Cs <aries0d0f@gmail.com>
2019-02-04 19:27:44 +0800

    Layout: bind Navbar component to Staff

--- src/views/Staff.vue
+++ src/views/Staff.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <Navbar />
     <!--  -->
   </div>
 </template>
+
+<script>
+import * as layout from '@/components'
+
+export default {
+  components: {
+    ...layout
+  }
+}
+</script>
