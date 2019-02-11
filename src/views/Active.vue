7ee664c2728bd447163d5fbab1317114ed3b29c7
Aries Cs <aries0d0f@gmail.com>
2019-02-11 11:32:58 +0800

    Layout: add active

--- src/views/Active.vue
+++ src/views/Active.vue
@@ -1,13 +1,39 @@
 <template>
   <div>
     <Navbar />
+    <div class="spotlight-container">
+      <div
+        v-for="(sponsor, index) in sponsors"
+        :key="index"
+        class="spotlight card"
+      >
+        <div class="card-container">
+          <div class="logo-image" :style="{ 'background-image': `url(../../static/img/sponsor/${sponsor.image})` }"></div>
+          <div class="content">
+            <h1>{{ sponsor.name }}</h1>
+            <p
+              v-for="(content, index) in sponsor.description"
+              :key="index"
+            >
+              {{ content }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import * as layout from '@/components'
+import sponsorData from '../../static/json/sponsor.json'
 
 export default {
+  data () {
+    return {
+      sponsors: sponsorData
+    }
+  },
   components: {
     ...layout
   }
