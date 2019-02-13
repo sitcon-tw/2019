36f1cb6735fbdb6a2f777b74e4c810a851bfd4ea
Aries Cs <aries0d0f@gmail.com>
2019-02-13 21:04:43 +0800

    Fix: popup close block z-index

--- src/components/SessionPopup.vue
+++ src/components/SessionPopup.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="session-popup">
-    <div
-      class="session-popup-close"
-      @click="$router.push({ name: 'Agenda' })"
-    >
-      <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjEyMDggOTA2IDI2IDI2Ij48ZGVmcz48c3R5bGU+LmF7ZmlsbDojZmZmO2ZpbGwtcnVsZTpldmVub2RkO308L3N0eWxlPjwvZGVmcz48cGF0aCBjbGFzcz0iYSIgZD0iTTMxLDcuNiwyOC40LDUsMTgsMTUuNCw3LjYsNSw1LDcuNiwxNS40LDE4LDUsMjguNCw3LjYsMzEsMTgsMjAuNiwyOC40LDMxLDMxLDI4LjQsMjAuNiwxOFoiIHRyYW5zZm9ybT0idHJhbnNsYXRlKDEyMDMgOTAxKSIvPjwvc3ZnPg==" alt="">
-    </div>
-    <div class="session-popup-container">
-      <div class="session-popup-content">
-        <h1>{{ data.zh.title }}</h1>
-        <p>{{ data.zh.description }}</p>
-      </div>
+    <div class="session-popup-placeholder">
       <div
-        v-for="speaker in data.speakers"
-        :key="speaker.id"
-        class="session-popup-content"
+        class="session-popup-close"
+        @click="$router.push({ name: 'Agenda' })"
       >
-        <h1>About {{ speaker.zh.name }}</h1>
-        <p><img :src="speaker.avatar" alt=""></p>
-        <p>{{ speaker.zh.bio }}</p>
+        <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjEyMDggOTA2IDI2IDI2Ij48ZGVmcz48c3R5bGU+LmF7ZmlsbDojZmZmO2ZpbGwtcnVsZTpldmVub2RkO308L3N0eWxlPjwvZGVmcz48cGF0aCBjbGFzcz0iYSIgZD0iTTMxLDcuNiwyOC40LDUsMTgsMTUuNCw3LjYsNSw1LDcuNiwxNS40LDE4LDUsMjguNCw3LjYsMzEsMTgsMjAuNiwyOC40LDMxLDMxLDI4LjQsMjAuNiwxOFoiIHRyYW5zZm9ybT0idHJhbnNsYXRlKDEyMDMgOTAxKSIvPjwvc3ZnPg==" alt="">
+      </div>
+      <div class="session-popup-container">
+        <div class="session-popup-content">
+          <h1>{{ data.zh.title }}</h1>
+          <p>{{ data.zh.description }}</p>
+        </div>
+        <div
+          v-for="speaker in data.speakers"
+          :key="speaker.id"
+          class="session-popup-content"
+        >
+          <h1>About {{ speaker.zh.name }}</h1>
+          <p><img :src="speaker.avatar" alt=""></p>
+          <p>{{ speaker.zh.bio }}</p>
+        </div>
       </div>
     </div>
   </div>
