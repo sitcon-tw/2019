b431bc30cda9af4236ee0f8ce907dcfd11df98ea
Aries Cs <aries0d0f@gmail.com>
2019-02-17 19:05:39 +0800

    Layout: add venue image

--- src/views/Venue.vue
+++ src/views/Venue.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <div class="spotlight-container">
+      <div class="spotlight">
+        <img src="@/assets/images/SVG/3F.svg" alt="">
+        <img src="@/assets/images/SVG/4F.svg" alt="">
+      </div>
+    </div>
     <Footer />
   </div>
 </template>
